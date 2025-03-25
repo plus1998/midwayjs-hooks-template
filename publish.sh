3b4eec343bbe6751853dc941b5c26349e2f8a456
happyplus <894860970@qq.com>
2025-03-26 02:36:43 +0800

    opt

--- publish.sh
+++ publish.sh
@@ -1,12 +1,29 @@
-# 将dist文件夹同步到服务器目录
+# 变量
+project_name="hooks_demo"
+username="ubuntu"
+host="xx.xx.xx.xx"
 
-# 在服务器上执行以下命令：
-# sudo chown -R ubuntu:ubuntu /home/ubuntu/hooks_demo
-# sudo chmod -R 755 /home/ubuntu/hooks_demo
+# 创建文件夹
+ssh $username@$host "mkdir -p /home/$username/$project_name"
+
+# 非root用户
+ssh $username@$host "sudo chown -R $username:$username /home/$username/$project_name && sudo chmod -R 755 /home/$username/$project_name"
+
+# 将package.json文件同步到服务器
+scp package.json $username@$host:/home/$username/$project_name/package.json
+
+# npm install
+ssh $username@$host "cd /home/$username/$project_name && npm install --omit=dev"
 
 # 输出同步开始
 echo "同步开始"
-rsync -avz --delete dist/ root@xx.xx.xx.xx:/home/ubuntu/hooks_demo/dist/
+rsync -avz --delete dist/ $username@$host:/home/$username/$project_name/dist/
 
 # 输出同步完成
 echo "同步完成"
+
+# pm2 部署
+ssh $username@$host "cd /home/$username/$project_name && pm2 start -x npm --name=$project_name -- run start"
+
+# pm2 重启
+# ssh $username@$host "pm2 restart $project_name"
