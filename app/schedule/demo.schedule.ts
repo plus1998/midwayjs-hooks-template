abcc07d7c2b958420d6b71a3e7c92d9de67507fb
happyplus <894860970@qq.com>
2025-03-26 23:43:09 +0800

    fix

--- app/schedule/demo.schedule.ts
+++ app/schedule/demo.schedule.ts
@@ -2,7 +2,7 @@ import { Processor, IProcessor } from '@midwayjs/bullmq';
 import { FORMAT, Inject } from '@midwayjs/core';
 import { MidwayLogger } from '@midwayjs/logger';
 
-@Processor('demo', {
+@Processor('demo.schedule', {
   repeat: {
     pattern: FORMAT.CRONTAB.EVERY_PER_5_SECOND,
   },
