e9ef155d212f865d9dc2e95dfd774fc1940b13d6
plus1998 <plus.1998@qq.com>
2025-07-20 13:43:05 +0800

    fix: jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['<rootDir>/app/test/fixtures'],
-    coveragePathIgnorePatterns: ['<rootDir>/app/test/'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/app/test/fixtures'],
+  coveragePathIgnorePatterns: ['<rootDir>/app/test/'],
+  moduleNameMapper: {
+    '^@entity/(.*)$': '<rootDir>/app/entity/$1',
+  },
+};
